e7a0463d41cb88f4820095afc1309cbe4d156747
Nghia <nguyenphuocnghia2369@gmail.com>
2026-08-04 15:01:54 +0700

    test(auth): implement unit tests for login and refresh token
    - Add FluentAssertions package
    - Create MockDbContextFactory for in-memory DB testing
    - Add unit tests for LoginCommand & LoginCommandValidator
    - Add unit tests for RefreshTokenCommand & RefreshTokenCommandValidato

---
+++ PayrollManagementSystem/backend/tests/PayrollManagementSystem.UnitTests/Mocks/MockDbContextFactory.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using PayrollManagementSystem.Application.Common.Interfaces;
+using PayrollManagementSystem.Infrastructure.Persistence;
+using Moq;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace PayrollManagementSystem.UnitTests.Mocks
+{
+    public static class MockDbContextFactory
+    {
+        public static ApplicationDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+
+            var context = new ApplicationDbContext(options);
+
+            context.Database.EnsureCreated();
+            return context;
+        }
+        
+        public static void Destroy(ApplicationDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
